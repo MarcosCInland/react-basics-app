4e8d7662a6a5dcf120dd16f1c1497360aeb2255a
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-09-18 23:03:39 -0600

    conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,26 +1,30 @@
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
 import { useState } from 'react';
 import './Expenses.css'
+import ExpensesList from './ExpensesList';
 function Expenses (props) {
     //Save updated year
     const [filteredYear, setFilteredYear] = useState('2022');
 
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear)
-        //console.log(selectedYear);
     }
+    //Filter by year
     const filteredExpenses = props.expenses.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
+    //After filtering, validate what to display
+    /*let expensesContent = '';
+    filteredExpenses.length > 0 ? expensesContent = <p>No expenses found</p>
+    : expensesContent = filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />);*/
+    
     return (
         <Card className='expenses'>
             <div>
                 <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             </div>
-            {filteredExpenses.length === 0 ? <p>No expenses found</p>
-            : (filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />))}
+            <ExpensesList expenses={filteredExpenses} />
         </Card>
     );
 }
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,16 @@
+import './ExpensesList.css';
+import ExpenseItem from './ExpenseItem';
+
+const ExpensesList = (props) => {
+    //After filtering, validate what to display
+    if(props.expenses.length === 0){
+        return <h2 className='expenses-list__fallback'>Found no expenses.</h2>;
+    }
+
+    return(
+        <ul className='expenses-list'>
+            {props.expenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)}
+        </ul>
+    );
+};
+export default ExpensesList;
